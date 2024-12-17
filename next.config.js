f8c996fd1c67cb07f5fb94690396418c4fa35f96
matthewasloan95 <matthewasloan95@gmail.com>
2024-12-17 13:38:43 -0500

    update configs

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/matthewasloan95.github.io', // Replace with your actual repo name
     images: {
       unoptimized: true,
     },
-  }
+}
   
-  module.exports = nextConfig
+module.exports = nextConfig
