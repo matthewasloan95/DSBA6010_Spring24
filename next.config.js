6d8ee02e637c64f1a4b9b0dcab0dade34b6e6fd9
matthewasloan95 <matthewasloan95@gmail.com>
2024-12-17 13:44:13 -0500

    Moving to course-specific repository

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/DSBA6010_Spring24',  // Add this line
     images: {
       unoptimized: true,
     },
