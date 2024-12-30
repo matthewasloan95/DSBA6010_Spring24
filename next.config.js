c2897edd6d42709c1e180724b4f5312f6c6c7205
matthewasloan95 <matthewasloan95@gmail.com>
2024-12-30 16:20:00 -0500

    remove comment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/DSBA6010_Spring24',  // Add this line
+    basePath: '/DSBA6010_Spring24',  
     images: {
       unoptimized: true,
     },
